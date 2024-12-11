1bd742519f720f857994684d29c88a0ed3578625
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-12-11 17:11:42 +0000

    Data fetched and stored in browser

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
-import { TextField, InputAdornment, Container } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
+import React, { useState, useEffect } from "react";
+import { TextField, InputAdornment, Container } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import axios from "axios";
 
 const SearchBar = () => {
+  // const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchResults = await axios.get("https://fakestoreapi.com/products");
+        const dataResults = fetchResults.data; // axios already parses JSON
+        console.log(dataResults);
+        setResults(dataResults);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        console.error(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <h5>...loading</h5>;
+  }
+
+  if (error) {
+    return <h5>Error: {error.message}</h5>;
+  }
+
   return (
-    <Container style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-      <div style={{ width: '300px' }}>
+    <Container
+      style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+    >
+      <div style={{ width: "300px" }}>
         <TextField
           label="Search"
           variant="outlined"
@@ -17,6 +50,7 @@ const SearchBar = () => {
               </InputAdornment>
             ),
           }}
+          onChange={(e) => setQuery(e.target.value)}
         />
       </div>
     </Container>
