f542f1359309ba31080a8f2f30fbf8898b6b59b6
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-12-12 16:58:19 +0000

    Search and suggestion features added

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,45 +1,48 @@
 import React, { useState, useEffect } from "react";
-import { TextField, InputAdornment, Container } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
 import axios from "axios";
-import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
 
 const SearchBar = () => {
   // const [query, setQuery] = useState("");
-  const [resultsSearch, setResultsSearch] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestion, setSuggestion] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const fetchResults = await axios.get(
-          "https://fakestoreapi.com/products?query="
-        );
-        const dataResults = fetchResults.data;
-        console.log(dataResults);
-        setResultsSearch(dataResults);
-        setLoading(false);
-      } catch (err) {
-        setError(err);
-        console.error(err);
-      }
-    };
+    if (searchTerm.trim() === "") {
+      setSuggestion([]);
+    } else {
+      fetchSuggestion(searchTerm);
+    }
+  }, [searchTerm]);
 
-    fetchData();
-  }, []);
+  const fetchSuggestion = async () => {
+    try {
+      const fetchResults = await axios.get(
+        "https://fakestoreapi.com/products?query="
+      );
+      const dataResults = fetchResults.data;
+      console.log(dataResults);
+      setResultsSearch(dataResults);
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+      console.error(err);
+    }
+  };
 
-  const handleOnSearch = (string, results)=>{
-    console.log(string, results)
-  }
+  const handleOnSearch = (string, results) => {
+    console.log(string, results);
+  };
 
-  const handleOnHover = (item)=>{
-console.log("Item hovered:", item)
-  }
+  const handleOnHover = (item) => {
+    console.log("Item hovered:", item);
+  };
 
-  const handleOnSelect = (item)=>{
-    console.log("Item hovered:",item)
-  }
+  const handleOnSelect = (item) => {
+    console.log("Item hovered:", item);
+  };
 
   if (loading) {
     return <h5>...loading</h5>;
@@ -54,13 +57,12 @@ console.log("Item hovered:", item)
       style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
     >
       <div style={{ width: "300px" }}>
-        
         <ReactSearchAutocomplete
-        items={resultsSearch}
-        onSearch={handleOnSearch}
-        onSelect={handleOnSelect}
-        onHover={handleOnHover}
-        placeholder="Type search"
+          items={resultsSearch}
+          onSearch={handleOnSearch}
+          onSelect={handleOnSelect}
+          onHover={handleOnHover}
+          placeholder="Type search"
         />
       </div>
     </Container>
